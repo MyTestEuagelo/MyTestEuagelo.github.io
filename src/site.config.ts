309d463094b0dc5838b893cd9622e15f227df045
MyTestEuagelo <euagelobill2@gmail.com>
2026-01-05 02:58:18 +0200

    My Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Vasileios Evangelopoulos",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "en-GB",
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "My website",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Bill Bo",
+	title: "Vasileios Evangelopoulos",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
 	url: "https://test-euagelo-site.netlify.app",
 };
